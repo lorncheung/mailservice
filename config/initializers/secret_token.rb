077b70f5216ac46203addaa698a3f56e3afb42d3
Loren Cheung <loren@taskrabbit.com>
2012-07-13 00:58:55 -0700

    initial rails 3 project check in

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mailservice::Application.config.secret_token = '4a53da3add6bbd33223c7d9a120c5b57a08209921a6a9204a97a48b473a2cfc510adcb1f114ebda0e71462c86bd3897734b24d2c9c7f4353dff860e13eca7f41'
